59ad4134f9c60b3860f8465172314827182d8cdf
Qiwei Huang <qiwei_huang@qq.com>
2022-05-07 16:30:13 +0800

    update format style

--- output_image/main.cpp
+++ output_image/main.cpp
@@ -15,8 +15,8 @@ int main() {
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining:" << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      Color pixel_color(double(i) / (image_width - 1),
-                        double(j) / (image_height - 1), 0.5f);
+      Color pixel_color(static_cast<double>(i) / (image_width - 1),
+                        static_cast<double>(j) / (image_height - 1), 0.5f);
       write_color(std::cout, pixel_color);
     }
   }
