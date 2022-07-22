0d689746ad50fa5938c7179411d73d0527156779
Qiwei Huang <qiwei_huang@qq.com>
2022-07-23 00:34:09 +0800

    reset to 1 thread

--- dielectric/main.cpp
+++ dielectric/main.cpp
@@ -1,7 +1,6 @@
 #include <iomanip>
 #include <iostream>
 
-#include "utility/BS_thread_pool.hpp"
 #include "utility/camera.h"
 #include "utility/color.h"
 #include "utility/hittable_list.h"
@@ -33,42 +32,12 @@ Color ray_color(const Ray& r, const Hittable& world, int depth) {
   }
 }
 
-void render(std::vector<Color>* color_buffer, int j, const int image_width,
-            const int image_height, const Camera& camera, int samples_per_pixel,
-            const HittableList world, const int max_depth,
-            std::atomic_uint32_t* line_count, BS::synced_stream* out) {
-  BS::timer tmr;
-  tmr.start();
-  for (int i = 0; i < image_width; ++i) {
-    Color pixel_color(0, 0, 0);
-    for (int s = 0; s < samples_per_pixel; ++s) {
-      auto u = (i + random_double()) / (image_width - 1);
-      auto v = (j + random_double()) / (image_height - 1);
-
-      Ray ray = camera.GetRay(u, v);
-      pixel_color += ray_color(ray, world, max_depth);
-    }
-    (*color_buffer)[(image_height - j) * image_width + i] =
-        pixel_color / samples_per_pixel;
-  }
-
-  (*line_count)++;
-
-  tmr.stop();
-  std::cerr << "The elapsed time was " << tmr.ms() << " ms.\n";
-
-  out->print(
-      "\rPercentage:" +
-      std::to_string(*line_count / static_cast<double>(image_height) * 100) +
-      "%");
-}
-
 int main() {
   // Image
   const auto aspect_ratio = 16.0 / 9.0;
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
-  const int samples_per_pixel = 1000;
+  const int samples_per_pixel = 200;
   const int max_depth = 100;
 
   // World
@@ -90,29 +59,19 @@ int main() {
   // Render
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-  std::vector<Color> color_buffer(image_width * image_height);
+  for (int j = image_height - 1; j >= 0; --j) {
+    std::cerr << "\rScanlines remaining:" << j << ' ' << std::flush;
+    for (int i = 0; i < image_width; ++i) {
+      Color pixel_color(0, 0, 0);
+      for (int s = 0; s < samples_per_pixel; ++s) {
+        auto u = (i + random_double()) / (image_width - 1);
+        auto v = (j + random_double()) / (image_height - 1);
 
-  BS::thread_pool pool(1);
-
-  std::atomic_uint32_t line_count(0);
-
-  BS::synced_stream sync_out(std::cerr);
-
-  BS::timer tmr;
-  tmr.start();
-
-  std::vector<std::thread> threads;
-
-  for (int j = 0; j < image_height; ++j) {
-    pool.push_task(render, &color_buffer, j, image_width, image_height, camera,
-                   samples_per_pixel, world, max_depth, &line_count, &sync_out);
+        Ray ray = camera.GetRay(u, v);
+        pixel_color += ray_color(ray, world, max_depth);
+      }
+      write_color(std::cout, pixel_color, samples_per_pixel);
+    }
   }
-  pool.wait_for_tasks();
-
-  for (size_t i = 0; i < image_width * image_height; i++) {
-    write_color(std::cout, color_buffer[i]);
-  }
-
-  tmr.stop();
-  std::cerr << "The elapsed time was " << tmr.ms() / 1000 << " s.\n";
+  std::cerr << "\nDone.\n";
 }
