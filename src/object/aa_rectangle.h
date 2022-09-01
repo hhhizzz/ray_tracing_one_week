1bb0eb9e2c87bd15c77cbfe1d4e34c5984e2d112
Qiwei Huang <qiwei_huang@qq.com>
2022-09-01 19:35:53 +0800

    Add cornell box

--- src/object/aa_rectangle.h
+++ src/object/aa_rectangle.h
@@ -24,7 +24,55 @@ class XyRectangle : public Hittable {
 
  private:
   std::shared_ptr<Material> material_;
-  double x0_, x1_, y0_, y1_, k_;
+  double x0_{}, x1_{}, y0_{}, y1_{}, k_{};
+};
+
+class XzRectangle : public Hittable {
+ public:
+  XzRectangle() = default;
+  XzRectangle(double x0, double x1, double z0, double z1, double k,
+              std::shared_ptr<Material> mat)
+      : x0_(x0), x1_(x1), z0_(z0), z1_(z1), k_(k), material_(std::move(mat)) {}
+
+  [[nodiscard]] bool Hit(const Ray& r, double t_min, double t_max,
+                         HitRecord* rec) const override;
+
+  [[nodiscard]] bool BoundingBox(double time0, double time1,
+                                 Aabb* output_box) const override {
+    // The bounding box must have non-zero width in each dimension, so pad the
+    // Y dimension a small amount.
+    *output_box =
+        Aabb(Point3(x0_, k_ - 0.0001, z0_), Point3(x1_, k_ + 0.0001, z1_));
+    return true;
+  }
+
+ private:
+  std::shared_ptr<Material> material_;
+  double x0_{}, x1_{}, z0_{}, z1_{}, k_{};
+};
+
+class YzRectangle : public Hittable {
+ public:
+  YzRectangle() = default;
+  YzRectangle(double y0, double y1, double z0, double z1, double k,
+              std::shared_ptr<Material> mat)
+      : y0_(y0), y1_(y1), z0_(z0), z1_(z1), k_(k), material_(std::move(mat)) {}
+
+  [[nodiscard]] bool Hit(const Ray& r, double t_min, double t_max,
+                         HitRecord* rec) const override;
+
+  [[nodiscard]] bool BoundingBox(double time0, double time1,
+                                 Aabb* output_box) const override {
+    // The bounding box must have non-zero width in each dimension, so pad the
+    // X dimension a small amount.
+    *output_box =
+        Aabb(Point3(k_ - 0.0001, y0_, z0_), Point3(k_ + 0.0001, y1_, z1_));
+    return true;
+  }
+
+ private:
+  std::shared_ptr<Material> material_;
+  double y0_{}, y1_{}, z0_{}, z1_{}, k_{};
 };
 
 bool XyRectangle::Hit(const Ray& r, double t_min, double t_max,
@@ -45,6 +93,48 @@ bool XyRectangle::Hit(const Ray& r, double t_min, double t_max,
   hit_record->SetFaceNormal(r, outward_normal);
   hit_record->material = material_;
   hit_record->p = r.At(t);
+  return true;
+}
+
+bool XzRectangle::Hit(const Ray& r, double t_min, double t_max,
+                      HitRecord* rec) const {
+  auto t = (k_ - r.Origin().Y()) / r.Direction().Y();
+  if (t < t_min || t > t_max) {
+    return false;
+  }
+  auto x = r.Origin().X() + t * r.Direction().X();
+  auto z = r.Origin().Z() + t * r.Direction().Z();
+  if (x < x0_ || x > x1_ || z < z0_ || z > z1_) {
+    return false;
+  }
+  rec->u = (x - x0_) / (x1_ - x0_);
+  rec->v = (z - z0_) / (z1_ - z0_);
+  rec->t = t;
+  auto outward_normal = Vec3(0, 1, 0);
+  rec->SetFaceNormal(r, outward_normal);
+  rec->material = material_;
+  rec->p = r.At(t);
+  return true;
+}
+
+bool YzRectangle::Hit(const Ray& r, double t_min, double t_max,
+                      HitRecord* rec) const {
+  auto t = (k_ - r.Origin().X()) / r.Direction().X();
+  if (t < t_min || t > t_max) {
+    return false;
+  }
+  auto y = r.Origin().Y() + t * r.Direction().Y();
+  auto z = r.Origin().Z() + t * r.Direction().Z();
+  if (y < y0_ || y > y1_ || z < z0_ || z > z1_) {
+    return false;
+  }
+  rec->u = (y - y0_) / (y1_ - y0_);
+  rec->v = (z - z0_) / (z1_ - z0_);
+  rec->t = t;
+  auto outward_normal = Vec3(1, 0, 0);
+  rec->SetFaceNormal(r, outward_normal);
+  rec->material = material_;
+  rec->p = r.At(t);
   return true;
 }
 
---
+++ src/object/rotate.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "object/hittable.h"
+
+class RotateY : public Hittable {
+ public:
+  RotateY(std::shared_ptr<Hittable> p, double angle);
+
+  virtual bool Hit(const Ray& r, double t_min, double t_max,
+                   HitRecord* rec) const override;
+
+  virtual bool BoundingBox(double time0, double time1,
+                           Aabb* output_box) const override {
+    *output_box = bbox_;
+    return has_box_;
+  }
+
+ public:
+  std::shared_ptr<Hittable> ptr_;
+  double sin_theta_;
+  double cos_theta_;
+  bool has_box_;
+  Aabb bbox_;
+};
+
+RotateY::RotateY(std::shared_ptr<Hittable> p, double angle) : ptr_(p) {
+  ptr_ = p;
+  auto radians = DegreesToRadians(angle);
+  sin_theta_ = std::sin(radians);
+  cos_theta_ = std::cos(radians);
+  has_box_ = ptr_->BoundingBox(0, 1, &bbox_);
+
+  Point3 min(infinity, infinity, infinity);
+  Point3 max(-infinity, -infinity, -infinity);
+
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      for (int k = 0; k < 2; k++) {
+        auto x = i * bbox_.Maximum().X() + (1 - i) * bbox_.Minimum().X();
+        auto y = j * bbox_.Maximum().Y() + (1 - j) * bbox_.Minimum().Y();
+        auto z = k * bbox_.Maximum().Z() + (1 - k) * bbox_.Minimum().Z();
+
+        auto newX = cos_theta_ * x + sin_theta_ * z;
+        auto newZ = -sin_theta_ * x + cos_theta_ * z;
+
+        Vec3 tester(newX, y, newZ);
+
+        for (int c = 0; c < 3; c++) {
+          min[c] = fmin(min[c], tester[c]);
+          max[c] = fmax(max[c], tester[c]);
+        }
+      }
+    }
+  }
+  bbox_ = Aabb(min, max);
+}
+
+bool RotateY::Hit(const Ray& r, double t_min, double t_max,
+                  HitRecord* rec) const {
+  auto origin = r.Origin();
+  auto direction = r.Direction();
+
+  origin[0] = cos_theta_ * r.Origin()[0] - sin_theta_ * r.Origin()[2];
+  origin[2] = sin_theta_ * r.Origin()[0] + cos_theta_ * r.Origin()[2];
+
+  direction[0] = cos_theta_ * r.Direction()[0] - sin_theta_ * r.Direction()[2];
+  direction[2] = sin_theta_ * r.Direction()[0] + cos_theta_ * r.Direction()[2];
+
+  Ray rotated_r(origin, direction, r.Time());
+
+  if (!ptr_->Hit(rotated_r, t_min, t_max, rec)) {
+    return false;
+  }
+
+  auto p = rec->p;
+  auto normal = rec->normal;
+
+  p[0] = cos_theta_ * rec->p[0] + sin_theta_ * rec->p[2];
+  p[2] = -sin_theta_ * rec->p[0] + cos_theta_ * rec->p[2];
+
+  normal[0] = cos_theta_ * rec->normal[0] + sin_theta_ * rec->normal[2];
+  normal[2] = -sin_theta_ * rec->normal[0] + cos_theta_ * rec->normal[2];
+
+  rec->p = p;
+  rec->SetFaceNormal(rotated_r, normal);
+
+  return true;
+}
+
+#pragma endregion
