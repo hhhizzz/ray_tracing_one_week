9d68c10f5ff7ac0c35e95f22deaf3c4156c0a057
Qiwei Huang <qiwei_huang@qq.com>
2022-09-04 13:37:36 +0800

    add next week final scene

--- src/main.cpp
+++ src/main.cpp
@@ -243,6 +243,79 @@ HittableList CornellSmoke(std::shared_ptr<Camera>& camera) {
   return objects;
 }
 
+HittableList TheNextWeek(std::shared_ptr<Camera>& camera) {
+  HittableList boxes1;
+  auto ground = make_shared<Lambertian>(Color(0.48, 0.83, 0.53));
+
+  const int boxes_per_side = 20;
+  for (int i = 0; i < boxes_per_side; i++) {
+    for (int j = 0; j < boxes_per_side; j++) {
+      auto w = 100.0;
+      auto x0 = -1000.0 + i * w;
+      auto z0 = -1000.0 + j * w;
+      auto y0 = 0.0;
+      auto x1 = x0 + w;
+      auto y1 = RandomDouble(1, 101);
+      auto z1 = z0 + w;
+
+      boxes1.Add(
+          make_shared<Box>(Point3(x0, y0, z0), Point3(x1, y1, z1), ground));
+    }
+  }
+
+  HittableList objects;
+
+  objects.Add(make_shared<BvhNode>(boxes1, 0, 1));
+
+  auto light = make_shared<DiffuseLight>(Color(7, 7, 7));
+  objects.Add(make_shared<XzRectangle>(123, 423, 147, 412, 554, light));
+
+  auto center1 = Point3(400, 400, 200);
+  auto center2 = center1 + Vec3(30, 0, 0);
+  auto moving_sphere_material = make_shared<Lambertian>(Color(0.7, 0.3, 0.1));
+  objects.Add(make_shared<MovingSphere>(center1, center2, 0, 1, 50,
+                                        moving_sphere_material));
+
+  objects.Add(make_shared<Sphere>(Point3(260, 150, 45), 50,
+                                  make_shared<Dielectric>(1.5)));
+  objects.Add(make_shared<Sphere>(
+      Point3(0, 150, 145), 50, make_shared<Metal>(Color(0.8, 0.8, 0.9), 1.0)));
+
+  auto boundary = make_shared<Sphere>(Point3(360, 150, 145), 70,
+                                      make_shared<Dielectric>(1.5));
+  objects.Add(boundary);
+  objects.Add(make_shared<ConstantMedium>(boundary, 0.2, Color(0.2, 0.4, 0.9)));
+  boundary =
+      make_shared<Sphere>(Point3(0, 0, 0), 5000, make_shared<Dielectric>(1.5));
+  objects.Add(make_shared<ConstantMedium>(boundary, .0001, Color(1, 1, 1)));
+
+  auto e_mat = make_shared<Lambertian>(
+      make_shared<ImageTexture>("resources/earth-map.jpg"));
+  objects.Add(make_shared<Sphere>(Point3(400, 200, 400), 100, e_mat));
+  auto per_text = make_shared<NoiseTexture>(0.1);
+  objects.Add(make_shared<Sphere>(Point3(220, 280, 300), 80,
+                                  make_shared<Lambertian>(per_text)));
+
+  HittableList boxes2;
+  auto white = make_shared<Lambertian>(Color(.73, .73, .73));
+  int ns = 1000;
+  for (int j = 0; j < ns; j++) {
+    boxes2.Add(make_shared<Sphere>(Point3::Random(0, 165), 10, white));
+  }
+
+  objects.Add(make_shared<Translate>(
+      make_shared<RotateY>(make_shared<BvhNode>(boxes2, 0.0, 1.0), 15),
+      Vec3(-100, 270, 395)));
+
+  auto new_camera = std::make_shared<Camera>(
+      Point3(478, 278, -600), Point3(278, 278, 0), camera->v_up_, 40, 1.0,
+      camera->aperture_, camera->focus_dist_, Color(0, 0, 0), 0, 1);
+
+  camera = new_camera;
+
+  return objects;
+}
+
 int main(int argc, char** argv) {
   // Camera
   auto aspect_ratio = 16.0 / 9.0;
@@ -268,6 +341,7 @@ int main(int argc, char** argv) {
       {"SampleLight", SampleLight(camera)},
       {"CornellBox", CornellBox(camera)},
       {"CornellSmoke", CornellSmoke(camera)},
+      {"TheNextWeek", TheNextWeek(camera)},
   };
 
   // Image
